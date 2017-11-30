a368259fe50d4e8550a4b539fa46d79fd9f037fb
Lee Skillen <lskillen@cloudsmith.io>
2017-11-30 15:15:40 +0000

    Add additional hints to display for 401/404/500 codes

--- cloudsmith_cli/cli/exceptions.py
+++ cloudsmith_cli/cli/exceptions.py
@@ -23,8 +23,7 @@ def handle_api_exceptions(
             click.echo()
             click.secho('ERROR: ', fg='red', nl=False)
         else:
-            click.secho('ERROR', fg='red', nl=False)
-            click.echo()
+            click.secho('ERROR', fg='red')
 
         context_msg = context_msg or 'Failed to perform operation!'
         click.secho(
@@ -57,6 +56,14 @@ def handle_api_exceptions(
                     }
                 )
 
+        hint = get_error_hint(ctx, opts, exc)
+        if hint:
+            click.secho(
+                'Hint: %(hint)s' % {
+                    'hint': click.style(hint, fg='yellow')
+                }
+            )
+
         if opts.verbose and not opts.debug:
             if exc.headers:
                 click.echo()
@@ -74,3 +81,45 @@ def handle_api_exceptions(
 
         if exit_on_error:
             ctx.exit(exc.status)
+
+
+def get_error_hint(ctx, opts, exc):
+    """Get a hint to show to the user (if any)."""
+    module = sys.modules[__name__]
+    get_specific_error_hint = (
+        getattr(module, 'get_%s_error_hint' % exc.status, None))
+    if get_specific_error_hint:
+        return get_specific_error_hint(ctx, opts, exc)
+    return None
+
+
+def get_401_error_hint(ctx, opts, exc):
+    """Get the hint for a 401/Unauthorised error."""
+    # pylint: disable=unused-argument
+    if opts.api_key:
+        return (
+            'Since you have an API key set, this probably means '
+            'you don\'t have the permision to perform this action.')
+    return (
+        'You don\'t have an API key set, but it seems this action '
+        'requires authenticated - Try getting your API key via '
+        '`cloudsmith token` first then try again.')
+
+
+def get_404_error_hint(ctx, opts, exc):
+    """Get the hint for a 404/NotFound error."""
+    # pylint: disable=unused-argument
+    # pylint: disable=fixme
+    # TODO(ls): Expand this to be contextual (we could look at the
+    # arguments for the command).
+    return 'This usually means the user/org is wrong or not visible.'
+
+
+def get_500_error_hint(ctx, opts, exc):
+    """Get the hint for a 500/InternalServerError error."""
+    # pylint: disable=unused-argument
+    return (
+        'This usually means the Cloudsmith service is encountering '
+        'issues, either with this specific command or as a whole. '
+        'Please accept our apologies and try again later.'
+    )
