70a0aa0c59eb487632b9802eea79977cd4080789
Lee Skillen <lskillen@cloudsmith.io>
2019-02-13 15:12:27 +0000

    Removed old git hooks

--- .git-hooks/post_checkout/pip_install.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../shared/pip_install'
-
-module Overcommit::Hook::PostCheckout
-  # Runs `pip install` when a change is detected in the repository's
-  # dependencies.
-  class PipInstall < Base
-    include Overcommit::Hook::Shared::PipInstall
-  end
-end
