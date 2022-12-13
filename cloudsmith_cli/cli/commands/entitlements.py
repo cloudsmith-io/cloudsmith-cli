642e284c0574e29963b861bda2eadfce4a19c135
Jack Gibson <jgibson@cloudsmith.io>
2022-12-13 12:19:57 +0000

    Handle relevant entitlement datetime fields.

--- cloudsmith_cli/cli/commands/entitlements.py
+++ cloudsmith_cli/cli/commands/entitlements.py
@@ -132,6 +132,9 @@ def print_entitlements(opts, data, page_info=None, show_list_info=True):
 
     rows = []
     for entitlement in sorted(data, key=itemgetter("name")):
+        ent_updated_at = entitlement["updated_at"].strftime("%Y/%m/%d, %H:%M:%S") if entitlement["updated_at"] else None
+        ent_created_at = entitlement["created_at"].strftime("%Y/%m/%d, %H:%M:%S") if entitlement["created_at"] else None
+        
         rows.append(
             [
                 click.style(
@@ -143,7 +146,7 @@ def print_entitlements(opts, data, page_info=None, show_list_info=True):
                 ),
                 click.style(entitlement["token"], fg="yellow"),
                 click.style(
-                    entitlement["updated_at"] or entitlement["created_at"], fg="blue"
+                   ent_updated_at or ent_created_at, fg="blue"
                 ),
                 click.style(entitlement["slug_perm"], fg="green"),
             ]
@@ -189,10 +192,12 @@ def print_entitlements_with_restrictions(
 
     rows = []
     for entitlement in sorted(data, key=itemgetter("name")):
+
+
         name = entitlement.get("name", "")
         user = entitlement.get("user", "")
-        updated_at = entitlement.get("updated_at", "")
-        created_at = entitlement.get("created_at", "")
+        updated_at = entitlement.get("updated_at").strftime("%Y/%m/%d, %H:%M:%S") if entitlement["updated_at"] else ""
+        created_at = entitlement.get("created_at").strftime("%Y/%m/%d, %H:%M:%S") if entitlement["created_at"] else ""
         is_active = entitlement.get("is_active", "")
         is_limited = entitlement.get("is_limited", "")
 
@@ -201,8 +206,8 @@ def print_entitlements_with_restrictions(
         limit_bandwidth_unit = entitlement.get("limit_bandwidth_unit", "")
         limit_num_clients = entitlement.get("limit_num_clients", "")
         limit_num_downloads = entitlement.get("limit_num_downloads", "")
-        limit_date_range_from = entitlement.get("limit_date_range_from", "")
-        limit_date_range_to = entitlement.get("limit_date_range_to", "")
+        limit_date_range_from = entitlement.get("limit_date_range_from").strftime("%Y/%m/%d, %H:%M:%S") if entitlement["limit_date_range_from"] else ""
+        limit_date_range_to = entitlement.get("limit_date_range_to").strftime("%Y/%m/%d, %H:%M:%S") if entitlement["limit_date_range_to"] else ""
         limit_package_query = entitlement.get("limit_package_query", "")
         limit_path_query = entitlement.get("limit_path_query", "")
 
