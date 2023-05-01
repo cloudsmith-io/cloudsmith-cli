69655d74c172e1a5ef09daa79036874ad18d3895
Jack Gibson <jgibson@cloudsmith.io>
2023-05-01 11:25:22 +0100

    fix: Update generation of format parameter descriptions.

--- cloudsmith_cli/core/api/packages.py
+++ cloudsmith_cli/core/api/packages.py
@@ -255,7 +255,7 @@ def get_package_formats():
         for k, v in six.iteritems(cls.swagger_types):
             attr = getattr(cls, k)
             docs = attr.__doc__.strip().split("\n")
-            doc = (docs[1] if docs[1] else docs[0]).strip()
+            doc = (docs[2].lstrip() if docs[2] else docs[0]).strip()
 
             try:
                 setattr(instance, k, None)
