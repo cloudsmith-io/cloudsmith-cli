974893d97ff45b89f70282ceae1f4ae89e4ee0da
Chris McEvoy <12284065+chrisimcevoy@users.noreply.github.com>
2023-05-03 11:37:59 +0100

    Add tests for login command (#105)
    * Add tests for login command

---
+++ cloudsmith_cli/cli/tests/commands/test_login.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from ...commands.login import login
+
+
+@pytest.mark.usefixtures("set_api_host_env_var")
+class TestLoginCommand(object):
+    def test_login_via_prompt(self, runner, username, password, api_key):
+        """Test that a user can `cloudsmith login` with interactive prompts."""
+        expected = "Your API key/token is: %s" % api_key
+        user_input = [
+            username,  # Login:
+            password,  # Password:
+            password,  # Repeat for confirmation:
+            "N",  # No default config file(s) found, do you want to create them? [y/N]:
+        ]
+        result = runner.invoke(login, input="\n".join(user_input))
+        assert not result.exception
+        assert result.exit_code == 0
+        assert expected in result.stdout
+
+    def test_login_via_args(self, runner, username, password, api_key):
+        """Test that a user can `cloudsmith login -l <login> -p <password>`."""
+        expected = "Your API key/token is: %s" % api_key
+        # The "input" argument here answers the following prompt:
+        # No default config file(s) found, do you want to create them? [y/N]:
+        result = runner.invoke(login, ["-l", username, "-p", password], input="N\n")
+        assert not result.exception
+        assert result.exit_code == 0
+        assert expected in result.stdout
---
+++ cloudsmith_cli/cli/tests/conftest.py
@@ -0,0 +1,48 @@
+import os
+
+import click.testing
+import pytest
+
+
+def _get_env_var_or_skip(key):
+    """Return the environment variable value if set, otherwise skip the test."""
+    value = os.environ.get(key)
+    if not value:
+        pytest.skip("%s not provided" % key)
+    return value
+
+
+@pytest.fixture()
+def runner():
+    """Return a CliRunner with which to run Commands."""
+    return click.testing.CliRunner()
+
+
+@pytest.fixture()
+def username():
+    """Return the PYTEST_CLOUDSMITH_USERNAME value."""
+    return _get_env_var_or_skip("PYTEST_CLOUDSMITH_USERNAME")
+
+
+@pytest.fixture()
+def password():
+    """Return the PYTEST_CLOUDSMITH_PASSWORD value."""
+    return _get_env_var_or_skip("PYTEST_CLOUDSMITH_PASSWORD")
+
+
+@pytest.fixture()
+def api_key():
+    """Return the PYTEST_CLOUDSMITH_API_KEY value."""
+    return _get_env_var_or_skip("PYTEST_CLOUDSMITH_API_KEY")
+
+
+@pytest.fixture()
+def api_host():
+    """Return the PYTEST_CLOUDSMITH_API_HOST value."""
+    return _get_env_var_or_skip("PYTEST_CLOUDSMITH_API_HOST")
+
+
+@pytest.fixture()
+def set_api_host_env_var(api_host):
+    """Set the CLOUDSMITH_API_HOST environment variable."""
+    os.environ["CLOUDSMITH_API_HOST"] = api_host
