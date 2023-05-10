b75b7e2c145253ac42b34850b2925a131c3e7d1b
Lee O'Connell <loconnell@cloudsmith.io>
2023-05-10 09:53:32 +0100

    feat(eng-2261): License policy CLI

--- cloudsmith_cli/cli/commands/policy/__init__.py
+++ cloudsmith_cli/cli/commands/policy/__init__.py
@@ -1,2 +1,3 @@
 # -*- coding: utf-8 -*-
 from .vulnerability import vulnerability as policy_vulnerability  # noqa
+from .license import license  as policy_license # noqa
--- cloudsmith_cli/core/api/orgs.py
+++ cloudsmith_cli/core/api/orgs.py
@@ -65,3 +65,55 @@ def delete_vulnerability_policy(owner, slug_perm):
         )
 
     ratelimits.maybe_rate_limit(client, headers)
+
+
+def list_license_policies(owner, page, page_size):
+    """List license policies in a namespace."""
+    client = get_orgs_api()
+
+    with catch_raise_api_exception():
+        policies, _, headers = client.orgs_license_policy_list_with_http_info(
+            org=owner, page=page, page_size=page_size
+        )
+
+    ratelimits.maybe_rate_limit(client, headers)
+    page_info = PageInfo.from_headers(headers)
+    return [policy.to_dict() for policy in policies], page_info
+
+
+def create_license_policy(owner, policy_config):
+    """Create a license policy in a namespace."""
+    client = get_orgs_api()
+
+    with catch_raise_api_exception():
+        policy, _, headers = client.orgs_license_policy_create_with_http_info(
+            org=owner, data=policy_config
+        )
+
+    ratelimits.maybe_rate_limit(client, headers)
+    return policy.to_dict()
+
+
+def update_license_policy(owner, slug_perm, policy_config):
+    """Update a license policy in a namespace."""
+    client = get_orgs_api()
+
+    with catch_raise_api_exception():
+        policy, _, headers = client.orgs_license_policy_partial_update_with_http_info(
+            org=owner, slug_perm=slug_perm, data=policy_config
+        )
+
+    ratelimits.maybe_rate_limit(client, headers)
+    return policy.to_dict()
+
+
+def delete_license_policy(owner, slug_perm):
+    """Delete a license policy from a namespace."""
+    client = get_orgs_api()
+
+    with catch_raise_api_exception():
+        _, _, headers = client.orgs_license_policy_delete_with_http_info(
+            owner, slug_perm
+        )
+
+    ratelimits.maybe_rate_limit(client, headers)
