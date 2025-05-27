f385f18ce1cc02c0ed0223492b7d42b2e04c1b30
Kieran Manning <kmanning@cloudsmith.io>
2025-05-27 20:15:23 +0100

    adding tests

--- cloudsmith_cli/cli/tests/commands/test_main.py
+++ cloudsmith_cli/cli/tests/commands/test_main.py
@@ -6,17 +6,14 @@ from ...commands.main import main
 
 
 class TestMainCommand:
-    @pytest.mark.usefixtures("set_no_warn_env_var")
     @pytest.mark.parametrize("option", ["-V", "--version"])
     def test_main_version(self, runner, option):
         """Test the output of `cloudsmith --version`."""
         result = runner.invoke(main, [option])
         assert result.exit_code == 0
-        assert (
-            result.output == "Versions:\n"
-            "CLI Package Version: " + get_version() + "\n"
-            "API Package Version: " + get_api_version() + "\n"
-        )
+
+        assert "CLI Package Version: " + get_version() in result.output
+        assert "API Package Version: " + get_api_version() in result.output
 
     @pytest.mark.parametrize("option", ["-h", "--help"])
     def test_main_help(self, runner, option):
