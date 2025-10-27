010a677c839f1dca71f0eee8f4a1d35acd6a0285
colinmoynes <cmoynes@cloudsmith.io>
2025-10-27 17:06:47 +0000

    fix --json param for auth workflow

--- cloudsmith_cli/cli/commands/auth.py
+++ cloudsmith_cli/cli/commands/auth.py
@@ -16,18 +16,20 @@ AUTH_SERVER_HOST = "127.0.0.1"
 AUTH_SERVER_PORT = 12400
 
 
-def _perform_saml_authentication(opts, owner, enable_token_creation=False):
+def _perform_saml_authentication(opts, owner, enable_token_creation=False, json=False):
     """Perform SAML authentication via web browser and local web server."""
     session = create_configured_session(opts)
     api_host = opts.api_config.host
 
     idp_url = get_idp_url(api_host, owner, session=session)
-    click.echo(
-        f"Opening your organization's SAML IDP URL in your browser: {click.style(idp_url, bold=True)}"
-    )
+    if not json:
+        click.echo(
+            f"Opening your organization's SAML IDP URL in your browser: {click.style(idp_url, bold=True)}"
+        )
     click.echo()
     webbrowser.open(idp_url)
-    click.echo("Starting webserver to begin authentication ... ")
+    if not json:
+        click.echo("Starting webserver to begin authentication ... ")
 
     auth_server = AuthenticationWebServer(
         (AUTH_SERVER_HOST, AUTH_SERVER_PORT),
@@ -86,13 +88,16 @@ def authenticate(ctx, opts, owner, token, force, save_config, json):
     """Authenticate to Cloudsmith using the org's SAML setup."""
     owner = owner[0].strip("'[]'")
 
-    click.echo(
-        f"Beginning authentication for the {click.style(owner, bold=True)} org ... "
-    )
+    if not json:
+        click.echo(
+            f"Beginning authentication for the {click.style(owner, bold=True)} org ... "
+        )
 
     context_message = "Failed to authenticate via SSO!"
     with handle_api_exceptions(ctx, opts=opts, context_msg=context_message):
-        _perform_saml_authentication(opts, owner, enable_token_creation=token)
+        _perform_saml_authentication(
+            opts, owner, enable_token_creation=token, json=json
+        )
 
     if token:
         ctx.invoke(create, opts=opts, save_config=save_config, force=force, json=json)
