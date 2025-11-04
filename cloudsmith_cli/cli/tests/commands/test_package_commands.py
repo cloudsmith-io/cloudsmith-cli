469da97fe9d03443c6c45c889987434e1754dca0
colinmoynes <cmoynes@cloudsmith.io>
2025-11-04 08:44:55 +0000

    revert test code

--- cloudsmith_cli/cli/tests/commands/test_package_commands.py
+++ cloudsmith_cli/cli/tests/commands/test_package_commands.py
@@ -27,8 +27,6 @@ def test_push_and_delete_raw_package(
     result = runner.invoke(
         list_, args=["pkgs", org_repo, "-F", "json"], catch_exceptions=False
     )
-    if result is not None:
-        result = '{ "data":[] }'
     data = json.loads(result.output)["data"]
     assert len(data) == 0
 
