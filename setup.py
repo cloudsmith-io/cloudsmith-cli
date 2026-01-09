44b2b98ba60d7999048bbe95aa397bda8d2b136e
Esteban Garcia <egarcia@cloudsmith.io>
2026-01-09 11:04:17 +0000

    fix: deps

--- setup.py
+++ setup.py
@@ -55,6 +55,8 @@ setup(
         "click-spinner>=0.1.7",
         "cloudsmith-api>=2.0.22,<3.0",  # Compatible upto (but excluding) 3.0+
         "keyring>=25.4.1",
+        "mcp==1.9.1",
+        "toon-python==0.1.2",
         "requests>=2.18.4",
         "requests_toolbelt>=1.0.0",
         "semver>=2.7.9",
